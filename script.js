3cbde5db6963d24a5033112249485186eee50fe1
Sahlberg96 <daniel.sahlberg@hotmail.com>
2026-09-11 14:31:56 +0200

    random citat

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ let timesClicked = 0;
 
 const button = document.getElementById('myFunction');
 const textRundor = document.getElementById("timesClicked");
+const quoteText = document.getElementById("quote");
+const quotes = ["Go Tisha!", "Team Tisha is the best!", "Keep up the great work, Team Tisha!", ];
 function buttonPressed() {
-    alert('Hail Team Tisha!'); 
+    
     timesClicked++;
     textRundor.textContent = timesClicked;
+    const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomQuoteIndex];
+    alert(randomQuote);
 }
 
 button.addEventListener('click', buttonPressed);
     
+
 
 
