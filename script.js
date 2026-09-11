867b8138de1e37310191985337f7c0f0590f3c3b
Sahlberg96 <daniel.sahlberg@hotmail.com>
2026-09-11 13:24:39 +0200

    button updated

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// 1. Hitta knappen i HTML-dokumentet med hjälp av dess id
+const knapp = document.getElementById('myFunction');
 
+function buttonPressed() {
+    alert('Kopplingen fungerar! Du klickade på knappen.');
+}
+
+knapp.addEventListener('click', buttonPressed);
