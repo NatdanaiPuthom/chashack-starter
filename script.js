83434633c7ad643a383680467287d08bfd4d54d7
Sahlberg96 <daniel.sahlberg@hotmail.com>
2026-09-11 14:05:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// 1. Hitta knappen i HTML-dokumentet med hjälp av dess id
+
 let timesClicked = 0;   
 
 const button = document.getElementById('myFunction');
 const textRundor = document.getElementById("timesClicked");
 function buttonPressed() {
-    alert('Kopplingen fungerar! Du klickade på knappen.');
+    alert('Hail Team Tisha!'); // 2. Visa ett alert-meddelande när knappen klickas
     timesClicked++;
     textRundor.textContent = timesClicked;
 }
