eedee08af2fa72c4e51c02cbb5f3f47400b2cbef
Natdanai Puthom <natdanai.puthom1997@gmail.com>
2026-09-11 14:32:33 +0200

    Update loggbok to fixed

--- LoadCommits.js
+++ LoadCommits.js
@@ -1,17 +1,55 @@
 const username = "NatdanaiPuthom";
 const repository = "chashack-starter";
 
-const url = `https://api.github.com/repos/${username}/${repository}/commits`;
+// Choose which commits to display
+const timelineCommits = [
+    {
+        title: "Projektet startades",
+        commit: "37e0516"
+    },
+    {
+        title: "Vi skapade hemsidan",
+        commit: "ad5785b"
+    },
+    {
+        title: "Vi lade till Tisha",
+        commit: "31c56d9"
+    },
+    {
+        title: "Vi lade till sidfot",
+        commit: "c58cde5"
+    },
+     {
+        title: "Vi lade till klick button tracker",
+        commit: "c14d9f8"
+    },
+    ,
+    {
+        title: "Vi lade till layout/containers",
+        commit: "0409bc3"
+    },
+    {
+        title: "Vi lade till Tisha logo",
+        commit: "1455269"
+    }
+    ,
+    {
+        title: "Vi lade till loggboken",
+        commit: "f096cd6"
+    }
+];
 
-fetch(url)
-    .then(response => response.json())
-    .then(commits => {
+const timeline = document.getElementById("commit-timeline");
 
-        const timeline = document.getElementById("commit-timeline");
+timeline.innerHTML = "";
 
-        timeline.innerHTML = "";
+timelineCommits.forEach(entry => {
 
-        commits.forEach(commit => {
+    const url = `https://api.github.com/repos/${username}/${repository}/commits/${entry.commit}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(commit => {
 
             const item = document.createElement("div");
             item.classList.add("timeline-item");
@@ -19,24 +57,17 @@ fetch(url)
             const date = new Date(commit.commit.author.date);
 
             item.innerHTML = `
-                <h3>${commit.commit.message}</h3>
+                <h3>${entry.title}</h3>
 
                 <p>
                     ${date.toLocaleDateString("sv-SE")}
                     ${date.toLocaleTimeString("sv-SE")}
                 </p>
-
-                <p>
-                    Commit:
-                    <a href="${commit.html_url}" target="_blank">
-                        ${commit.html_url}
-                    </a>
-                </p>
             `;
 
             timeline.appendChild(item);
+        })
+        .catch(error => {
+            console.error("Kunde inte hämta commit:", error);
         });
-    })
-    .catch(error => {
-        console.error("Kunde inte hämta commits:", error);
-    });
+});
